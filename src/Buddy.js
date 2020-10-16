cef79e85011f5d47c65682e79d7d13b163ffa305
Fabian Alleblas <fabian@alleblas.de>
2020-10-16 21:00:54 +0200

    Update Team.js Buddy.js

--- src/Buddy.js
+++ src/Buddy.js
@@ -3,10 +3,18 @@ import './Students.css'
 
 export default function Buddy({ target }){
     const el = createElement({ type: 'main', className: 'main-scrolling scrolling-80 p-3 grid-20', target })
-    const studentCard = createElement({ type: 'section', target: el})
-    const studentsList = createElement({ type: 'ul', className: 'Students__list Students__pairing', target: studentCard })
-    studentsList.innerHTML = `
-    <li>Angela Longoria</li>
-    <li>Maria Trofimova</li>
-    ` 
+    pairList({ student1: 'Sudanka Bakalowits', student2: 'Yasaman Foroutan' })
+    pairList({ student1: 'Sudanka Bakalowits', student2: 'Yasaman Foroutan' })
+    pairList({ student1: 'Sudanka Bakalowits', student2: 'Yasaman Foroutan' })
+    pairList({ student1: 'Sudanka Bakalowits', student2: 'Yasaman Foroutan' })
+
+    function pairList({ student1, student2 }){
+        const studentCard = createElement({ type: 'section', target: el})
+        const studentsList = createElement({ type: 'ul', className: 'Students__list Students__pairing', target: studentCard })
+        studentsList.innerHTML = `
+        <li>${student1}</li>
+        <li>${student2}</li>
+        ` 
+    }
 }
+
