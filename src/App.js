e27b8f3ce0eecf89d8e84e63c6333cc44c0fb924
Nina Peters <peters.nina@outlook.de>
2020-10-18 21:28:45 +0200

    Add Dashboard Header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css'
 
 import createElement from './createElement'
 
-// import Header from './Header'
+import Header from './Header'
 import Navigation from './Navigation'
 // import Button from './Button'
 import Energy from './Energy'
@@ -21,6 +21,8 @@ import EnergyIcon from './assets/EnergyIcon.svg'
 
 export default function App({ target }) {
     const el = createElement({ className: 'App' })
+
+    Header({ text: 'Dashboard', target: el})
 
 
 const navItems = [
--- src/Header.js
+++ src/Header.js
@@ -1,2 +1,38 @@
 import createElement from './createElement'
 import './Header.css'
+import BackgroundShape1 from './assets/background_shape_1-line.svg'
+//import BackgroundShape2 from './assets/background_shape_2-lines.svg'
+
+export default function Header({ target, text }) {
+    const el = createElement({ type:'header', className: 'Header-60 center', target})
+
+    update(text)
+
+    return { update }
+
+function update(text) {
+
+    el.innerHTML = `
+    <img class="Header-60">${BackgroundShape1}</img> 
+    <h1 class="Header__heading">${text}</h1>
+    `
+    }    
+}
+
+
+
+/* el.setAttribute('style', `'background-image: ${BackgroundShape1};background-size: cover;`)
+    */
+/* el.style.backgroundImage = BackgroundShape1
+el.style.backgroundSize = 'cover' */
+
+/* 
+
+el.innerHTML = `
+<img class="Header-60" style="background-image: ${BackgroundShape1}; background-size: cover;" >Header</img> 
+`
+<body style="font-weight:bold;">
+
+
+
+*/
