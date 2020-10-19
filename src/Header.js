ef572ea7e3e714b5391296f7d328435a431db8fb
Alexander Jonas <alex.jonas@gmail.com>
2020-10-19 15:13:14 +0200

    Update header.js

--- src/Header.js
+++ src/Header.js
@@ -1,38 +1,18 @@
 import createElement from './createElement'
 import './Header.css'
-import BackgroundShape1 from './assets/background_shape_1-line.svg'
-//import BackgroundShape2 from './assets/background_shape_2-lines.svg'
 
-export default function Header({ target, text }) {
-    const el = createElement({ type:'header', className: 'Header-60 center', target})
+export default function Header({ target, title, subTitle }) {
+    const el = createElement({ type:'header', className: 'Header center', target})
 
-    update(text)
+    update(title, subTitle)
 
     return { update }
 
-function update(text) {
-
+function update(title, subTitle = '') {
     el.innerHTML = `
-    <img class="Header-60">${BackgroundShape1}</img> 
-    <h1 class="Header__heading">${text}</h1>
+    <h1 class="Header__heading">${title}</h1>
+    <h2 class="Header__subheading">${subTitle}</h2>
     `
+    subTitle.length > 0 ? el.classList.add('Header-80') : el.classList.remove('Header-80')
     }    
 }
-
-
-
-/* el.setAttribute('style', `'background-image: ${BackgroundShape1};background-size: cover;`)
-    */
-/* el.style.backgroundImage = BackgroundShape1
-el.style.backgroundSize = 'cover' */
-
-/* 
-
-el.innerHTML = `
-<img class="Header-60" style="background-image: ${BackgroundShape1}; background-size: cover;" >Header</img> 
-`
-<body style="font-weight:bold;">
-
-
-
-*/
