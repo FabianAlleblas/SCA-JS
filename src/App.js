0976d8a0003db3c29d630d58843c5c70f6d74aec
Fabian Alleblas <fabian@alleblas.de>
2020-10-19 21:50:34 +0200

    Update app and navigation.js

--- src/App.js
+++ src/App.js
@@ -17,57 +17,70 @@ import TeamIcon from './assets/TeamIcon.svg'
 import JournalIcon from './assets/JournalIcon.svg'
 import EnergyIcon from './assets/EnergyIcon.svg'
 
-
 export default function App({ target }) {
-    const el = createElement({ className: 'App' })
+  const el = createElement({ className: 'App' })
 
-const navItems = [
-
+  const navItems = [
     {
-        path: 'dashboard',
-        component: Dashboard({target: el }),
-        icon: DashboardIcon,
-        title: 'Dashboard',
+      path: 'dashboard',
+      component: Dashboard({ target: el }),
+      icon: DashboardIcon,
+      title: 'Dashboard',
     },
     {
-        path: 'buddys',
-        component: Buddy({target: el }),
-        icon: BuddyIcon,
-        title: 'Code Buddys',
-        subtitle: 'On Monday - 18.07.2020'
+      path: 'buddys',
+      component: Buddy({ target: el }),
+      icon: BuddyIcon,
+      title: 'Code Buddys',
+      subtitle: 'On Monday - 18.07.2020',
     },
     {
-        path: 'teams',
-        component: Teams({target: el }),
-        icon: TeamIcon,
-        title: 'Teams',
-        subtitle: 'for Exercise 1'
+      path: 'teams',
+      component: Teams({ target: el }),
+      icon: TeamIcon,
+      title: 'Teams',
+      subtitle: 'for Exercise 1',
     },
     {
-        path: 'energy',
-        component: Energy({target: el }),
-        icon: EnergyIcon,
-        title: 'Energy',
+      path: 'energy',
+      component: Energy({ target: el }),
+      icon: EnergyIcon,
+      title: 'Energy',
     },
     {
-        path: 'journal',
-        component: Journal({target: el }),
-        icon: JournalIcon,
-        title: 'Journal',
+      path: 'journal',
+      component: Journal({ target: el }),
+      icon: JournalIcon,
+      title: 'Journal',
     },
-    
-    ]
+  ]
 
-const header = Header({ title: navItems[0].title, target: el})
+  const header = Header({ title: navItems[0].title, target: el })
 
-JournalForm({ target: el })
-Navigation({ target: el, navItems, onNavigate: handleNavigate })
+  JournalForm({ target: el })
+  Navigation({
+    target: el,
+    navItems,
+    onNavigate: handleNavigate,
+    activateNavButton,
+  })
 
-function handleNavigate(path) {
+  function handleNavigate(path) {
     const newNavItem = navItems.find((navItem) => navItem.path === path)
     header.update(newNavItem.title, newNavItem.subtitle)
-    navItems.forEach((navItem) => 
-    navItem === newNavItem ? navItem.component.show() : navItem.component.hide())
-}
-}
+    navItems.forEach((navItem) =>
+      navItem === newNavItem
+        ? navItem.component.show()
+        : navItem.component.hide()
+    )
+  }
 
+  function activateNavButton(path, navBar) {
+    const buttonSelector = navBar.querySelectorAll('button')
+    buttonSelector.forEach((button) =>
+      button.name === path
+        ? button.classList.add('Navigation__link--active')
+        : button.classList.remove('Navigation__link--active')
+    )
+  }
+}
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,27 +1,37 @@
 import createElement from './createElement'
 import './Navigation.css'
 
-export default function Navigation({target, onNavigate, navItems}) {
+export default function Navigation({
+  target,
+  onNavigate,
+  navItems,
+  activateNavButton,
+}) {
+  const el = createElement({
+    type: 'footer',
+    className: 'Navigation__container',
+    target,
+  })
+  const navBar = createElement({
+    type: 'nav',
+    className: 'Navigation',
+    target: el,
+  })
 
-const el = createElement({ type: 'footer', className: 'Navigation__container', target})
-const navBar = createElement({ type: 'nav', className: 'Navigation', target: el})
+  navItems.forEach((navItem) => {
+    const btn = createElement({
+      type: 'button',
+      target: navBar,
+      className: 'Navigation__link w-100',
+    })
+    btn.innerHTML = navItem.icon
+    btn.name = navItem.path
 
-navItems.forEach((navItem) => {
+    btn.addEventListener('click', () => {
+      onNavigate(navItem.path)
+      activateNavButton(navItem.path, navBar)
+    })
+  })
 
-const btn = createElement({ type: 'button', target: navBar, className: 'Navigation__link w-100' })
-btn.innerHTML = navItem.icon
-
-btn.addEventListener('click', (event) => {
-    onNavigate(navItem.path)
-    activateButton(event)
-})
-
-function activateButton(event){
-    const buttonSelector = navBar.querySelectorAll('button')
-    buttonSelector.forEach((button) => button === event.currentTarget ? button.classList.add('Navigation__link--active') : button.classList.remove('Navigation__link--active'))
-}
-
-})
-
-return {}
+  return {}
 }
