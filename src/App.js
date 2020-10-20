88975dc8b7d67e0cd06f3f463976020f83985b1c
Nina Peters <peters.nina@outlook.de>
2020-10-20 21:04:05 +0200

    Update Journal Form

--- src/App.js
+++ src/App.js
@@ -18,79 +18,57 @@ import TeamIcon from './assets/TeamIcon.svg'
 import JournalIcon from './assets/JournalIcon.svg'
 import EnergyIcon from './assets/EnergyIcon.svg'
 
-
 export default function App({ target }) {
-    const el = createElement({ className: 'App' })
+  const el = createElement({ className: 'App' })
 
-
-
-const navItems = [
-
+  const navItems = [
     {
-        path: 'dashboard',
-        component: Dashboard({target: el, hidden: false }),
-        icon: DashboardIcon,
-        title: 'Dashboard',
+      path: 'dashboard',
+      component: Dashboard({ target: el, hidden: true }),
+      icon: DashboardIcon,
+      title: 'Dashboard',
     },
     {
-        path: 'buddys',
-        component: Buddy({target: el, hidden: true }),
-        icon: BuddyIcon,
-        title: 'Code Buddys',
-        subtitle: 'On Monday - 18.07.2020'
+      path: 'buddys',
+      component: Buddy({ target: el, hidden: true }),
+      icon: BuddyIcon,
+      title: 'Code Buddys',
+      subtitle: 'On Monday - 18.07.2020',
     },
     {
-        path: 'teams',
-        component: Teams({target: el, hidden: true }),
-        icon: TeamIcon,
-        title: 'Teams',
-        subtitle: 'for Exercise 1'
+      path: 'teams',
+      component: Teams({ target: el, hidden: true }),
+      icon: TeamIcon,
+      title: 'Teams',
+      subtitle: 'for Exercise 1',
     },
     {
-        path: 'energy',
-        component: Energy({target: el, hidden: true }),
-        icon: EnergyIcon,
-        title: 'Energy',
+      path: 'energy',
+      component: Energy({ target: el, hidden: true }),
+      icon: EnergyIcon,
+      title: 'Energy',
     },
     {
-        path: 'journal',
-        component: Journal({target: el, hidden: true }),
-        icon: JournalIcon,
-        title: 'Journal',
+      path: 'journal',
+      component: Journal({ target: el, hidden: true }),
+      icon: JournalIcon,
+      title: 'Journal',
     },
-    ]
+  ]
 
-const header = Header({ title: navItems[0].title, target: el})
+  const header = Header({ title: navItems[0].title, target: el })
 
-JournalForm({target: el, hidden: true, navigateToForm })
+  JournalForm({ target: el, hidden: false })
 
-Navigation( {target: el, navItems, onNavigate: handleNavigate })
+  Navigation({ target: el, navItems, onNavigate: handleNavigate })
 
-function handleNavigate(path) {
+  function handleNavigate(path) {
     const newNavItem = navItems.find((navItem) => navItem.path === path)
     header.update(newNavItem.title, newNavItem.subtitle)
     navItems.forEach((navItem) =>
-    navItem === newNavItem ? navItem.component.show() : navItem.component.hide())
+      navItem === newNavItem
+        ? navItem.component.show()
+        : navItem.component.hide()
+    )
+  }
 }
-
-/* function navigateToForm() {
-    const pageJournalForm = navItems.find(index === 5)
-    navItems.forEach((navItem) =>
-    navItem === pageJournalForm ? navItem.component.show() : navItem.component.hide())
-} */
-
-/* 
-    function navigateToForm() {
-        Dashboard.hide()
-        Buddy.hide()
-        Teams.hide()
-        Energy.hide()
-        Journal.hide()
-        JournalForm.show()
-    }
-
-
-*/
-
-}
-
--- src/JournalForm.js
+++ src/JournalForm.js
@@ -4,90 +4,216 @@ import RatingStar from './assets/star_outline.svg'
 import ComprehensionLevel from './assets/rectangle_outline.svg'
 import Button from './Buttons'
 
-export default function JournalForm({ target, hidden = true }) {
-   const el = createElement({ type: 'main', className: 'main-scrolling p-3', target})
-   const journalForm = createElement({ type: 'form', className: 'Journal-form', target: el })
-   const heading = createElement({ type: 'h3', className: 'Journal-form__heading mb-3', target: journalForm})
-    heading.textContent = 'Today, 12.10.2020'
-   
-    const ratingStars = createElement({ type:'fieldset', className: 'rating', target: journalForm})
-    subHeading({ type: 'legend', text: 'Rating:' })
-    const starsWrapper = createElement({className: 'stars', target: ratingStars})
-    
-   const ratingList = createElement({ type:'ul', className: 'Journal-form__rating mb-2', target: journalForm})
-    ratingList.innerHTML = `
-    <li>${RatingStar}</li> 
-    <li>${RatingStar}</li> 
-    <li>${RatingStar}</li> 
-    <li>${RatingStar}</li> 
-    <li>${RatingStar}</li> 
-    `
-    
+export default function JournalForm({ target, hidden = false }) {
+  const el = createElement({
+    type: 'main',
+    className: 'main-scrolling p-3',
+    target,
+  })
 
-  subHeading({ type: 'h4', text: 'Comprehension:' })
-  const comprehensionList = createElement({ type:'ul', className: 'Journal-form__comprehension mb-2', target: journalForm})
-  comprehensionList.innerHTML = `
-    <li>${ComprehensionLevel}</li>
-    <li>${ComprehensionLevel}</li>
-    <li>${ComprehensionLevel}</li>
-    <li>${ComprehensionLevel}</li>
-    <li>${ComprehensionLevel}</li>
-    <li>${ComprehensionLevel}</li>
-    <li>${ComprehensionLevel}</li>
-    <li>${ComprehensionLevel}</li>
-    <li>${ComprehensionLevel}</li>
-    <li>${ComprehensionLevel}</li>
-    `
+  const journalForm = createElement({
+    type: 'form',
+    className: 'Journal-form',
+    target: el,
+  })
 
-    const mottoSubHeading = subHeading({ type: 'label', text: 'Motto:' })
-    createElement({type: 'input', className: 'Journal-form__input w-100 mt-1 mb-2', target: mottoSubHeading })
+  const heading = createElement({
+    type: 'h3',
+    className: 'Journal-form__heading mb-3',
+    target: journalForm,
+  })
+  heading.textContent = 'Today, 12.10.2020'
 
-    const notesSubHeading = subHeading({ type: 'label', text: 'Notes:' })
-    createElement({ type: 'textarea', className: 'Journal-form__input w-100 mt-1 mb-2', target: notesSubHeading })
-        
-    const buttonWrapper = createElement({ className: 'Journal-form__button-wrapper grid-10 mt-1', target: journalForm })
-    Button({ text: 'Cancel', className: 'button plain', target: buttonWrapper })
-    Button({ text: 'Save ✓', className: 'button', target: buttonWrapper })
+  const ratingWrapper = createElement({
+    type: 'fieldset',
+    target: journalForm,
+  })
 
-    function subHeading({ type, text }){
-        const journalSubheading = createElement({type, className: 'Journal-form__subheading mb-1', target: journalForm})
-        journalSubheading.textContent = text
-        return (journalSubheading)
-    }
+  subHeading({ type: 'legend', text: 'Rating:', target: ratingWrapper })
 
-    function input({ value, id }) {
-        const el = createElement({type: 'input', target: starsWrapper})
-        el.setAttribute(type = 'radio')
-        el.setAttribute(name = 'rating')
-        el.setAttribute(value = `${value}`)
-        el.setAttribute(id = `rating${id}`)
-        console.log(el)
-        
-        /* 
-        const label = createElement({type: 'label', target: starsWrapper})
-        label.setAttribute(for = `rating${id}`)
+  const starWrapper = createElement({
+    target: ratingWrapper,
+    className: 'Journal-form__rating mb-2',
+  })
 
-        const div = createElement({className: 'star', target: label})
-        */
+  videoKilledTheRadioStar({
+    name: 'rating',
+    value: '5',
+    target: starWrapper,
+    icon: RatingStar,
+  })
+  videoKilledTheRadioStar({
+    name: 'rating',
+    value: '4',
+    target: starWrapper,
+    icon: RatingStar,
+  })
+  videoKilledTheRadioStar({
+    name: 'rating',
+    value: '3',
+    target: starWrapper,
+    icon: RatingStar,
+  })
+  videoKilledTheRadioStar({
+    name: 'rating',
+    value: '2',
+    target: starWrapper,
+    icon: RatingStar,
+  })
+  videoKilledTheRadioStar({
+    name: 'rating',
+    value: '1',
+    target: starWrapper,
+    icon: RatingStar,
+  })
 
-    }
+  const compWrapper = createElement({
+    type: 'fieldset',
+    target: journalForm,
+  })
 
-hidden && hide()
+  subHeading({ type: 'legend', text: 'Comprehension:', target: compWrapper })
 
-function show() {
+  const levelWrapper = createElement({
+    target: compWrapper,
+    className: 'Journal-form__comprehension mb-2',
+  })
+
+  videoKilledTheRadioStar({
+    name: 'comprehension',
+    value: '10',
+    target: levelWrapper,
+    icon: ComprehensionLevel,
+  })
+
+  videoKilledTheRadioStar({
+    name: 'comprehension',
+    value: '9',
+    target: levelWrapper,
+    icon: ComprehensionLevel,
+  })
+
+  videoKilledTheRadioStar({
+    name: 'comprehension',
+    value: '8',
+    target: levelWrapper,
+    icon: ComprehensionLevel,
+  })
+
+  videoKilledTheRadioStar({
+    name: 'comprehension',
+    value: '7',
+    target: levelWrapper,
+    icon: ComprehensionLevel,
+  })
+
+  videoKilledTheRadioStar({
+    name: 'comprehension',
+    value: '6',
+    target: levelWrapper,
+    icon: ComprehensionLevel,
+  })
+
+  videoKilledTheRadioStar({
+    name: 'comprehension',
+    value: '5',
+    target: levelWrapper,
+    icon: ComprehensionLevel,
+  })
+
+  videoKilledTheRadioStar({
+    name: 'comprehension',
+    value: '4',
+    target: levelWrapper,
+    icon: ComprehensionLevel,
+  })
+
+  videoKilledTheRadioStar({
+    name: 'comprehension',
+    value: '3',
+    target: levelWrapper,
+    icon: ComprehensionLevel,
+  })
+
+  videoKilledTheRadioStar({
+    name: 'comprehension',
+    value: '2',
+    target: levelWrapper,
+    icon: ComprehensionLevel,
+  })
+
+  videoKilledTheRadioStar({
+    name: 'comprehension',
+    value: '1',
+    target: levelWrapper,
+    icon: ComprehensionLevel,
+  })
+
+  const motto = subHeading({
+    type: 'label',
+    text: 'Motto:',
+    target: journalForm,
+  })
+  createElement({
+    type: 'input',
+    className: 'Journal-form__input w-100 mt-1 mb-2',
+    target: motto,
+  })
+
+  const notes = subHeading({
+    type: 'label',
+    text: 'Notes:',
+    target: journalForm,
+  })
+  createElement({
+    type: 'textarea',
+    className: 'Journal-form__input w-100 mt-1 mb-2',
+    target: notes,
+  })
+
+  const buttonWrapper = createElement({
+    className: 'Journal-form__button-wrapper grid-10 mt-1',
+    target: journalForm,
+  })
+  Button({ text: 'Cancel', className: 'button plain', target: buttonWrapper })
+  Button({ text: 'Save ✓', className: 'button', target: buttonWrapper })
+
+  function videoKilledTheRadioStar({
+    name,
+    value,
+    id = `${name}${value}`,
+    target,
+    icon,
+  }) {
+    const ratingBox = createElement({ type: 'input', target })
+    ratingBox.type = 'radio'
+    ratingBox.name = name
+    ratingBox.value = value
+    ratingBox.id = id
+    const iconLabel = createElement({ type: 'label', target })
+    iconLabel.setAttribute('for', id)
+    iconLabel.innerHTML = `${icon}`
+  }
+
+  function subHeading({ type, text, target }) {
+    const journalSubheading = createElement({
+      type,
+      className: 'Journal-form__subheading mb-1',
+      target: target,
+    })
+    journalSubheading.textContent = text
+    return journalSubheading
+  }
+
+  hidden && hide()
+
+  function show() {
     el.classList.remove('hidden')
-}
+  }
 
-function hide() {
+  function hide() {
     el.classList.add('hidden')
+  }
+
+  return { show, hide }
 }
-
-return {show, hide}
-
-
-}
-
-
-
-
-
