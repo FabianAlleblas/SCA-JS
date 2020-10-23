0b7e3161740c680b5e7eec7588d084f37120afc1
Alexander Jonas <alex.jonas@gmail.com>
2020-10-23 13:00:21 +0200

    Update stars/compr levels as array

--- src/JournalForm.js
+++ src/JournalForm.js
@@ -37,36 +37,27 @@ export default function JournalForm({ target, hidden = false, onCLick }) {
     className: 'Journal-form__rating mb-2',
   })
 
-  videoKilledTheRadioStar({
-    name: 'rating',
-    value: '5',
-    target: starWrapper,
-    icon: RatingStar,
-  })
-  videoKilledTheRadioStar({
-    name: 'rating',
-    value: '4',
-    target: starWrapper,
-    icon: RatingStar,
-  })
-  videoKilledTheRadioStar({
-    name: 'rating',
-    value: '3',
-    target: starWrapper,
-    icon: RatingStar,
-  })
-  videoKilledTheRadioStar({
-    name: 'rating',
-    value: '2',
-    target: starWrapper,
-    icon: RatingStar,
-  })
-  videoKilledTheRadioStar({
-    name: 'rating',
-    value: '1',
-    target: starWrapper,
-    icon: RatingStar,
-  })
+  const allRatingStars = Array(5)
+    .fill()
+    .map((_, index, arr) => arr.length - index)
+
+  allRatingStars.forEach((ratingStarNumber) =>
+    videoKilledTheRadioStar({
+      name: 'rating',
+      value: ratingStarNumber,
+      target: starWrapper,
+      //icon: RatingStar,
+      icon: `<svg width="36px" 
+                  height="35px" 
+                  stroke="#7589a2" 
+                  stroke-width=".75" 
+                  fill="none" 
+                  fill-rule="evenodd" 
+                  viewBox="-1 0 27 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                  <path d="M8.23458443,7.2288 L0.825542795,8.30177333 L0.715980417,8.32378638 C0.00767592863,8.50741356 -0.252463118,9.41029168 0.291855842,9.93923898 L5.65288072,15.14976 L4.3876206,22.5059207 L4.37507497,22.6116336 C4.32751461,23.3425174 5.11048682,23.8713631 5.78483118,23.5179301 L12.4119178,20.0448 L19.0390043,23.5179301 L19.1359768,23.5624921 C19.8185371,23.833441 20.5650032,23.2545037 20.4362149,22.5059207 L19.1699918,15.14976 L24.5319797,9.93923898 L24.6076386,9.85722905 C25.0724025,9.29362162 24.750523,8.41074247 23.9982927,8.30177333 L16.5882881,7.2288 L13.2754414,0.535136917 C12.9222134,-0.178378972 11.9016221,-0.178378972 11.5483941,0.535136917 L8.23458443,7.2288 Z" id="Path"></path>
+             </svg>`,
+    })
+  )
 
   const compWrapper = createElement({
     type: 'fieldset',
@@ -81,75 +72,26 @@ export default function JournalForm({ target, hidden = false, onCLick }) {
     className: 'Journal-form__comprehension mb-2',
   })
 
-  videoKilledTheRadioStar({
-    name: 'comprehension',
-    value: '10',
-    target: levelWrapper,
-    icon: ComprehensionLevel,
-  })
+  const allComprehensionLevels = Array(10)
+    .fill()
+    .map((_, index, arr) => arr.length - index)
 
-  videoKilledTheRadioStar({
-    name: 'comprehension',
-    value: '9',
-    target: levelWrapper,
-    icon: ComprehensionLevel,
-  })
-
-  videoKilledTheRadioStar({
-    name: 'comprehension',
-    value: '8',
-    target: levelWrapper,
-    icon: ComprehensionLevel,
-  })
-
-  videoKilledTheRadioStar({
-    name: 'comprehension',
-    value: '7',
-    target: levelWrapper,
-    icon: ComprehensionLevel,
-  })
-
-  videoKilledTheRadioStar({
-    name: 'comprehension',
-    value: '6',
-    target: levelWrapper,
-    icon: ComprehensionLevel,
-  })
-
-  videoKilledTheRadioStar({
-    name: 'comprehension',
-    value: '5',
-    target: levelWrapper,
-    icon: ComprehensionLevel,
-  })
-
-  videoKilledTheRadioStar({
-    name: 'comprehension',
-    value: '4',
-    target: levelWrapper,
-    icon: ComprehensionLevel,
-  })
-
-  videoKilledTheRadioStar({
-    name: 'comprehension',
-    value: '3',
-    target: levelWrapper,
-    icon: ComprehensionLevel,
-  })
-
-  videoKilledTheRadioStar({
-    name: 'comprehension',
-    value: '2',
-    target: levelWrapper,
-    icon: ComprehensionLevel,
-  })
-
-  videoKilledTheRadioStar({
-    name: 'comprehension',
-    value: '1',
-    target: levelWrapper,
-    icon: ComprehensionLevel,
-  })
+  allComprehensionLevels.forEach((comprehensionLevelNumber) =>
+    videoKilledTheRadioStar({
+      name: 'comprehension',
+      value: comprehensionLevelNumber,
+      target: levelWrapper,
+      icon: `<svg width="26px" 
+                  height="26px" 
+                  stroke="#7589a2" 
+                  stroke-width=".85" 
+                  fill="none" 
+                  fill-rule="evenodd" 
+                  viewBox="-.5 -.5 26 26" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <rect id="Rectangle-Copy-6" transform="translate(11.000000, 11.000000) rotate(-90.000000) translate(-11.000000, -11.000000) " x="0" y="0" width="22" height="22" rx="4"></rect>
+              </svg>`,
+    })
+  )
 
   const motto = subHeading({
     type: 'label',
